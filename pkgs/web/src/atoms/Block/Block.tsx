ddfd6db25647a3091d7add2e0653367b2d2449b2
zwesithu <withzues@gmail.com>
2025-01-07 22:42:55 +0630

    chore: block alignment, pkg refactor and apply prettier format

--- pkgs/web/src/atoms/Block/Block.tsx
+++ pkgs/web/src/atoms/Block/Block.tsx
@@ -1,33 +1,61 @@
 import { ReactNode } from "react";
 
 type BlockProps = {
-    [key: string]: any;
-    scale: "sm" | "md" | "lg" | "xl" | "2xl" | "full" | "screen" | "auto";
-    children: ReactNode;
+  [key: string]: any;
+  scale: "sm" | "md" | "lg" | "xl" | "2xl" | "full" | "screen" | "auto";
+  alignment: "center" | "left" | "right";
+  children: ReactNode;
 };
 
-export type BlockTailwindClasses = "border" | "border-2" | "border-4" | "border-8" | "border-16";
+export type BlockTailwindClasses =
+  | "border"
+  | "border-2"
+  | "border-4"
+  | "border-8"
+  | "border-16";
 
-const scaleClassesMap: Record<BlockProps['scale'], string> = {
-    full: "w-full h-full",
-    screen: "w-screen h-screen",
-    auto: "w-auto h-auto",
-    sm: "w-1/5 h-1/5",
-    md: "w-1/2 h-1/2",
-    lg: "w-3/5 h-3/5",
-    xl: "w-4/5 h-4/5",
-    "2xl": "w-11/12 h-4/5"
+const scaleClassesMap: Record<BlockProps["scale"], string> = {
+  full: "w-full h-full",
+  screen: "w-screen h-screen",
+  auto: "w-auto h-auto",
+  sm: "w-1/5 h-1/5",
+  md: "w-1/2 h-1/2",
+  lg: "w-3/5 h-3/5",
+  xl: "w-4/5 h-4/5",
+  "2xl": "w-11/12 h-4/5",
 };
 
-export function Block({ children, scale, ...props }: BlockProps) {
-    const scaleClasses = scaleClassesMap[scale] || "";
-    props.className = `${props.className || ""} ${scaleClasses}`.trim();
-    return <div {...props}>{children}</div>;
+const alignClassesMap: Record<BlockProps["alignment"], string> = {
+  center: "flex justify-center items-center",
+  left: "flex justify-start items-center",
+  right: "flex justify-end items-center",
+};
+
+export function Block({
+  children,
+  alignment = "left",
+  scale = "md",
+  ...props
+}: BlockProps) {
+  const scaleClasses = scaleClassesMap[scale] || "";
+  const alignClasses = alignClassesMap[alignment] || "";
+  props.className = `${
+    props.className || ""
+  } ${scaleClasses} ${alignClasses}`.trim();
+  return <div {...props}>{children}</div>;
 }
 
-export function ThemedBlock({ children, scale, ...props }: BlockProps) {
-    const scaleClasses = scaleClassesMap[scale] || "";
-    const themeClasses = "mugi:bg-straw dark:bg-gray-800";
-    props.className = `${props.className || ""} ${scaleClasses} ${themeClasses}`.trim();
-    return <div {...props}>{children}</div>;
+export function ThemedBlock({
+  children,
+  alignment = "left",
+  scale = "md",
+  ...props
+}: BlockProps) {
+  const scaleClasses = scaleClassesMap[scale] || "";
+  const alignClasses = alignClassesMap[alignment] || "";
+  const themeClasses = "mugi:bg-straw dark:bg-gray-800";
+  props.className = `${
+    props.className || ""
+  } ${scaleClasses} ${themeClasses} ${alignClasses}`.trim();
+  return <div {...props}>{children}</div>;
 }
