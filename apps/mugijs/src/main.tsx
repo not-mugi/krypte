dec593814ff933c4e2da2140b41de6e147be4c1e
ZweSithu <zwesithu@mugi-tech.com>
2025-01-13 10:53:41 +0630

    chore: update pkgs usage in apps

--- apps/mugijs/src/main.tsx
+++ apps/mugijs/src/main.tsx
@@ -1,14 +1,11 @@
 import App from "./App";
-import "@mugijs/web/styles.css";
+import "@mugijs/react-web/styles.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { WebThemeProvider } from "@mugijs/web";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <WebThemeProvider>
       <App />
-    </WebThemeProvider>
   </StrictMode>
 );
