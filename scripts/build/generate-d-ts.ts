6ea8a0bde5dfca1b8e1e9541fde49a1586d069ad
ZweSithu <withzues@gmail.com>
2025-01-15 14:07:33 +0630

    feat: compile inline types inside bundle with api-extractor for better ts autosuggestions for core pkgs

--- scripts/build/generate-d-ts.ts
+++ scripts/build/generate-d-ts.ts
@@ -2,10 +2,17 @@ import path from "path";
 import { $, usePowerShell } from "zx";
 
 export async function generateDts(pkgPath: string) {
-  let tsBuild = "tsconfig.build.json";
+  const apiExtractor = "api-extractor.json";
+  const tsBuild = "tsconfig.build.json";
   if (process.platform === "win32") {
     usePowerShell();
   }
   const tsconfig = path.resolve(pkgPath, tsBuild);
+  try {
   await $`yarn tsc -p ${tsconfig}`;
+  const extractorPath = path.resolve(pkgPath, apiExtractor);
+  await $`yarn api-extractor run --local --config ${extractorPath}`;
+  } catch (error) {
+    console.log(error) 
+  }
 }
