cb0a68cf2e50280b70e6b7726415476edbf4b572
zwesithu <withzues@gmail.com>
2025-01-16 23:32:13 +0630

    feat: introduce up quark to web react pkg in progress

--- pkgs/web/react/src/quarks/Up/index.tsx
+++ pkgs/web/react/src/quarks/Up/index.tsx
@@ -1,9 +1,41 @@
-export interface UpConfig {
-  size: object;
-  growth: object;
-  upwardPosition: object;
+import { Exchangable, HasIntereaction } from "..";
+import type { Display, Layout, Alignment, Justification } from "@mugijs/tailwind-core/types"
+import { displayBlueprint, layoutBlueprint, alignmentBlueprint, justificationBlueprint } from "@mugijs/tailwind-core/functions";
+
+export interface Upflavor extends HasIntereaction {
+  _display: Display;
+  _layout: Layout;
+  _alignment: Alignment;
+  _justification: Justification;
+
+  setDisplay(display: Display): void;
+  setLayout(layout: Layout): void;
+  setAlignment(alignment: Alignment): void;
+  setJustification(justification: Justification): void;
 }
 
-export default function useUp({}: UpConfig) {
-  const getWidthClasses = () => {};
+export class UpQuark extends Exchangable implements Upflavor {
+  _display: Display = displayBlueprint();
+  _layout: Layout = layoutBlueprint();
+  _justification: Justification = justificationBlueprint();
+  _alignment: Alignment = alignmentBlueprint();
+
+
+  public setDisplay(display: Display) {
+    this._display = display;
+  }
+
+  public setLayout(layout: Layout) {
+    this._layout = layout;
+  }
+
+  public setAlignment(alignment: Alignment) {
+    this._alignment = alignment;
+  }
+
+  public setJustification(justification: Justification) {
+    this._justification = justification;
+  }
+
+
 }
