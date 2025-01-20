99aa6e84b06e0f3f5bed49b90c91dc6451a0841d
zwesithu <withzues@gmail.com>
2025-01-20 18:52:03 +0630

    feat: cleaning artifacts before build pkgs

--- scripts/others/path.ts
+++ scripts/others/path.ts
@@ -1,5 +1,20 @@
 import path from "path";
+import { promises as fs } from "fs";
+import signale from "signale";
 
 export function getPath(fPath: string) {
   return path.posix.resolve(fPath);
+}
+
+export async function deleteArtifacts(
+  pkgPath: string,
+  directoriesToDelete: string[] = ["cjs", "dist", "esm", "lib"],
+  logger: { info: (msg : string) => void; warn: (msg : string) => void } = new signale.Signale({ scope: "scripts-path" })
+) {
+  for (const dir of directoriesToDelete) {
+    const dirPath = path.join(pkgPath, dir);
+    try {
+      await fs.rm(dirPath, { recursive: true, force: true });
+    } catch (_err) {}
+  }
 }
