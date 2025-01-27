44b7f45d65a29141e56df1ca299e8607198bb03a
ZweSithu <withzues@gmail.com>
2025-01-27 16:57:23 +0630

    fix: fix color getter returning wrong values

--- pkgs/core/tw/src/getters/mechanics/color.ts
+++ pkgs/core/tw/src/getters/mechanics/color.ts
@@ -7,31 +7,31 @@ import {
 
 /** @alpha */
 export const getTextColorClass = (color?: Colors) =>
-  color && color in COLOR_MAP ? `text-${COLOR_MAP[color]}` : "";
+  color && color in COLOR_MAP ? `${COLOR_MAP[color]}` : "";
 
 /** @alpha */
 export const getBackgroundColorClass = (color?: Colors) =>
-  color && color in BACKGROUND_COLOR_MAP ? `bg-${BACKGROUND_COLOR_MAP[color]}` : "";
+  color && color in BACKGROUND_COLOR_MAP ? `${BACKGROUND_COLOR_MAP[color]}` : "";
 
 /** @alpha */
 export const getBorderColorClass = (color?: Colors) =>
-  color && color in BORDER_COLOR_MAP ? `border-${BORDER_COLOR_MAP[color]}` : "";
+  color && color in BORDER_COLOR_MAP ? `${BORDER_COLOR_MAP[color]}` : "";
 
 /** @alpha */
 export const getBorderOutlineColorClass = (color?: Colors) =>
-  color && color in OUTLINE_COLOR_MAP ? `outline-${OUTLINE_COLOR_MAP[color]}` : "";
+  color && color in OUTLINE_COLOR_MAP ? `${OUTLINE_COLOR_MAP[color]}` : "";
 
 /** @alpha */
 export const getBorderRingColorClass = (color?: Colors) =>
-  color && color in RING_COLOR_MAP ? `ring-${RING_COLOR_MAP[color]}` : "";
+  color && color in RING_COLOR_MAP ? `${RING_COLOR_MAP[color]}` : "";
 
 /** @alpha */
 export const getTextDecorationColorClass = (color?: Colors) =>
-  color && color in DECORATION_COLOR_MAP ? `decoration-${DECORATION_COLOR_MAP[color]}` : "";
+  color && color in DECORATION_COLOR_MAP ? `${DECORATION_COLOR_MAP[color]}` : "";
 
 /** @alpha */
 export const getShadowColorClass = (color?: Colors) =>
-  color && color in SHADOW_COLOR_MAP ? `shadow-${SHADOW_COLOR_MAP[color]}` : "";
+  color && color in SHADOW_COLOR_MAP ? `${SHADOW_COLOR_MAP[color]}` : "";
 
 /** @alpha */
 export const ColorMechanics = {
